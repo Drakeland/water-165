cda919850cc26cfbed5e6ea7e837d277ea710a64
JamesPK <kennedy.jamesp@yahoo.com>
2016-05-27 12:57:51 -0700

    outlier coloring fixed by restricting color scale domain. color scheme changed to Red-Yellow-Blue

--- geomap.js
+++ geomap.js
@@ -3,20 +3,23 @@
 
 var format = function(d) {
     d = d / 1000;
-    return d3.format(',.02f')(d) + 'K';
+    return d3.format(',.02f')(d) + ' K';
 }
 
 var map = d3.geomap.choropleth()
     .geofile('countries.json')
-    .colors(colorbrewer.YlGnBu[9])
     .column('2013-2017')
+    .colors(colorbrewer.RdYlBu[9])
+    .domain([1000, 10000])
     .format(format)
     .legend(true)
-    .unitId('Country Code');
+    .unitId('Country Code')
+    .scale(0,100000);
 
 d3.csv('water_resources_1yr.csv', function(error, data) {
     console.log(data);
     d3.select('#map')
         .datum(data)
-        .call(map.draw, map);
+       .call(map.draw, map);
 });
+
