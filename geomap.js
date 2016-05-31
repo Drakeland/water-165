b6ee2b9b25e00149c2316f02aad656fc91e2c878
JamesPK <kennedy.jamesp@yahoo.com>
2016-05-31 16:18:04 -0700

    function created to change the year. year label added to html. year not yet user-changeable.

--- geomap.js
+++ geomap.js
@@ -6,6 +6,35 @@ var format = function(d) {
     return d3.format(',.02f')(d) + ' K';
 }
 
+//updates the year to display, changing colors and tooltip values
+var updateYear = function(newYear) {
+   d3.select('.yearlabel').text("Current year:" + newYear);
+   map.updateColumn(newYear);
+   
+   map.properties.column = newYear;
+
+   // Add new fill styles based on data values.
+   map.data.forEach(function (d) {
+       var uid = d[map.properties.unitId],
+           val = d[map.properties.column],
+           fill = map.colorScale(val);
+
+       // selectAll must be called and not just select, otherwise the data
+       // attribute of the selected path object is overwritten with map.data.
+       var unit = map.svg.selectAll('.' + map.properties.unitPrefix + '' + uid);
+
+       // Data can contain values for non existing units.
+       if (!unit.empty()) {
+           if (map.properties.duration) unit.transition().duration(map.properties.duration).style('fill', fill);else unit.style('fill', fill);
+
+           // New title with column and value.
+           var text = map.properties.unitTitle(unit.datum());
+           val = map.properties.format(val);
+           unit.select('title').text('' + text + '\n\n' + map.properties.column + ': ' + val);
+       }
+   });
+}
+
 var map = d3.geomap.choropleth()
     .geofile('countries.json')
     .column('2014')
@@ -14,7 +43,8 @@ var map = d3.geomap.choropleth()
     .format(format)
     .legend(true)
     .unitId('Country Code')
-    .scale(0,100000);
+    .scale(0,100000)
+    .duration('1000');
 
 d3.csv('water_res_full_formatted.csv', function(error, data) {
     console.log(data);
