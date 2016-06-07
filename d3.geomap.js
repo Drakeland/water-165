60a4c844e57a5c2e7c4577e078987450cfa23120
Drake Pitts <dpitts@ucsc.edu>
2016-06-06 17:00:44 -0700

    Revised tooltip styling, added directions

--- d3.geomap.js
+++ d3.geomap.js
@@ -4,52 +4,59 @@ var toolyears = [ "2014", "2012", "1992", "1982", "1972", "1962"];
 
 //returns string of tooltip info to be displayed
 function get_tool_text(country) {
-   var text = "Country: "+ country.Country+"<br>"; 
-   
-   text += get_avg_pct_changes(country);
-   
-   toolyears.filter(function(i) { return (country[i] != -1) }) //filter out years with no data
-            .forEach(function(yr) {
-               text+= ( yr + ": " + format(country[yr]) + "<br>");
-            });
+    var text = "<table id=\"tooltip-text\">";
 
-   return text;
+    text += "<tr><th>Country:</th><td>"+ country.Country+"</td></tr>";
+
+    text += get_avg_pct_changes(country);
+
+    toolyears.filter(function(i) { return (country[i] != -1) }) //filter out years with no data
+        .forEach(function(yr) {
+            text += "<tr><th>" + yr + ":</th><td>"
+                  + format(country[yr]) + "</td></tr>";
+        });
+
+    text += "</table>"
+    return text;
 }
 
 //calculates average annual change over 10, 20 and 50 year periods  
 function get_avg_pct_changes(country) {
    
-   var yr_1962 = country["1962"],
-       yr_1992 = country["1992"],
-       yr_2002 = country ["2002"], 
-       yr_2012 = country ["2012"];   
-   var pct_format = d3.format('.01f');
-   var chg_50, chg_20, chg_10;
+    var yr_1962 = country["1962"],
+        yr_1992 = country["1992"],
+        yr_2002 = country ["2002"], 
+        yr_2012 = country ["2012"];   
+    var pct_format = d3.format('.01f');
+    var chg_50, chg_20, chg_10;
    
-   chg_50= (yr_2012 - yr_1962) /yr_1962 * 100; //50 year % change
-   chg_50 = pct_format(chg_50 / 50) + "%"; //annualize and format %change
+    chg_50 = (yr_2012 - yr_1962) /yr_1962 * 100; //50 year % change
+    chg_50 = pct_format(chg_50 / 50) + "%"; //annualize and format %change
+    chg_50.replace('-', '&minus;'); // replace dash with n-dash 
 
-   chg_20 = (yr_2012 - yr_1992) /yr_1992 * 100; //25 year % change
-   chg_20 = pct_format(chg_20 / 20) + "%"; //annualize %change
+    chg_20 = (yr_2012 - yr_1992) /yr_1992 * 100; //25 year % change
+    chg_20 = pct_format(chg_20 / 20) + "%"; //annualize %change
+    chg_20.replace('-', '&minus;'); // replace dash with n-dash 
+    
+    chg_10 = (yr_2012 - yr_2002) /yr_2002 * 100; //50 year % change
+    chg_10 = pct_format(chg_10 / 10) + "%"; //annualize and format %change
+    chg_10.replace('-', '&minus;'); // replace dash with n-dash 
+    
+    if (yr_1962 == -1) {
+        chg_50 = "N/A"; //if no data exists from 1962, print "N/A" for 50 year
+    } 
+    if (yr_1992 == -1) {
+        chg_20 = "N/A"; //if no data exists from 1992, print "N/A" for 20 year
+    } 
+    if (yr_2002 == -1) {
+        chg_10 = "N/A"; //if no data exists from 2002, print "N/A" for 10 year
+    } 
 
-   chg_10 = (yr_2012 - yr_2002) /yr_2002 * 100; //50 year % change
-   chg_10 = pct_format(chg_10 / 10) + "%"; //annualize and format %change
-   
-   if (yr_1962 == -1) {
-      chg_50 = "N/A"; //if no data exists from 1962, print "N/A" for 50 year
-   } 
-   if (yr_1992 == -1) {
-      chg_20 = "N/A"; //if no data exists from 1992, print "N/A" for 20 year
-   } 
-   if (yr_2002 == -1) {
-      chg_10 = "N/A"; //if no data exists from 2002, print "N/A" for 10 year
-   } 
-   
-   var text_10_20_50 = "10-yr avg annual change: " + chg_10 + "<br>"
-                  + "20-yr avg annual change: " + chg_20 + "<br>"
-                  + "50-yr avg annual change: " + chg_50 + "<br>";
-   
-   return text_10_20_50;
+    var text_10_20_50 = "<tr><th><abbr title=\"10-year average annual change\">10-year &Delta;:</abbr></th><td>" + chg_10 + "</td></tr>";
+    text_10_20_50 += "<tr><th><abbr title=\"20-year average annual change\">20-year &Delta;:</abbr></th><td>" + chg_20 + "</td></tr>";
+    text_10_20_50 += "<tr><th><abbr title=\"50-year average annual change\">50-year &Delta;:</abbr></th><td>" + chg_50 + "</td></tr>";
+
+    return text_10_20_50;
    
 }
 function addAccessor(obj, name, value) {
@@ -548,14 +555,15 @@ var Choropleth = (function (_Geomap) {
                 if (!unit.empty()) {
                     if (self.properties.duration) unit.transition().duration(self.properties.duration).style('fill', fill);else unit.style('fill', fill);
 
-                    unit.on("mouseover", function(d) {                  
-                                          
-                        d3.select("#value").html(get_tool_text(country_obj) );
-                     })		
+                    unit.on("mouseover", function(d) {              
+                        d3.select("#value")
+                            .html(get_tool_text(country_obj));
+                        console.log(get_tool_text(country_obj));
+                     });
                      //define mousout tooltip behavior
-                    .on("mouseout", function(d) {	
-                              tool_div.classed("hidden", true);	
-                    })
+//                    .on("mouseout", function(d) {	
+//                        tool_div.classed("hidden", true);	
+//                    })
                 }
             });
 
